af8943e983ef3d9585864982da309974b9c2b58f
vetterocki <92949251+vetterocki@users.noreply.github.com>
2022-05-23 01:25:02 +0300

    Add files via upload

--- srm8/main.py
+++ srm8/main.py
@@ -2,65 +2,97 @@ import numpy as np
 from scipy.optimize import fsolve
 
 
-def function(variables):
-    x_1, x_2 = variables
+class NonLinearSystemSolver:
+    def __init__(self, x_values, epsilon):
+        self.__x_values = x_values
+        self.__epsilon = epsilon
+        self.__result = []
 
-    return [x_1 - np.cos(x_2) - 1,
-            x_2 - np.log10(x_1 + 7) - 10]
+    @property
+    def x_values(self):
+        return self.__x_values
 
+    @property
+    def epsilon(self):
+        return self.__epsilon
 
-def jacobian_matrix(variables):
-    x_1, x_2 = variables
-    return [[1, np.sin(x_2)],
-            [(-1 / (np.log(10) * (x_1 + 1))), 1]]
+    @epsilon.setter
+    def epsilon(self, epsilon):
+        self.__epsilon = epsilon
 
+    @x_values.setter
+    def x_values(self, x_values):
+        self.__x_values = x_values
 
-def newton(funct, x_values, jacobian, epsilon):
-    x_last = x_values
+    @staticmethod
+    def function(variables):
+        x_1, x_2 = variables
 
-    for k in range(50):
+        return [x_1 - np.cos(x_2) - 1,
+                x_2 - np.log10(x_1 + 7) - 10]
 
-        jacob = np.array(jacobian(x_last))
-        func = np.array(funct(x_last))
-
-        diff = np.linalg.solve(jacob, -func)
-        x_last = x_last + diff
-
-        if np.linalg.norm(diff) < epsilon:
-            break
-
-    return x_last
-
-
-def function_for_iter(x_1, x_2):
-    return x_1 - np.cos(x_2) - 1
-
-
-def f_1_for_iter(x1, x_2):
-    return 1 + np.cos(x_2)
+    @staticmethod
+    def jacobian_matrix(variables):
+        x_1, x_2 = variables
+        return [[1, np.sin(x_2)],
+                [(-1 / (np.log(10) * (x_1 + 1))), 1]]
 
 
-def f_2_for_iter(x_1, x2):
-    return 10 + np.log10(x_1 + 7)
+class Newton(NonLinearSystemSolver):
+    def __init__(self, x_values, epsilon):
+        super(Newton, self).__init__(x_values, epsilon)
+
+    def newton(self, funct=NonLinearSystemSolver.function, jacobian=NonLinearSystemSolver.jacobian_matrix):
+        x_last = self.x_values
+
+        for k in range(50):
+
+            jacob = np.array(jacobian(x_last))
+            func = np.array(funct(x_last))
+
+            diff = np.linalg.solve(jacob, -func)
+            x_last = x_last + diff
+            self.__result = x_last
+
+            if np.linalg.norm(diff) < self.epsilon:
+                break
+
+        return list(self.__result)
 
 
-def simple_iteration(x1, x2, eps=.001):
-    while abs(function_for_iter(x1, x2)) > eps:
-        x1, x2 = f_1_for_iter(x1, x2), f_2_for_iter(x1, x2)
+class SimpleIteration(NonLinearSystemSolver):
+    def __init__(self, x_values, epsilon):
+        super(SimpleIteration, self).__init__(x_values, epsilon)
 
-    return x1, x2
+    @staticmethod
+    def function_for_iter(x_1, x_2):
+        return x_1 - np.cos(x_2) - 1
+
+    @staticmethod
+    def f_1_for_iter(x1, x_2):
+        return 1 + np.cos(x_2)
+
+    @staticmethod
+    def f_2_for_iter(x_1, x2):
+        return 10 + np.log10(x_1 + 7)
+
+    def simple_iteration(self):
+        x1 = self.x_values[0]
+        x2 = self.x_values[1]
+        while abs(self.function_for_iter(x1, x2)) > self.epsilon:
+            x1, x2 = self.f_1_for_iter(x1, x2), self.f_2_for_iter(x1, x2)
+            self.__result = [x1, x2]
+
+        return self.__result
 
 
-def iter_(x1, x2):
-    x1, x2 = simple_iteration(x1, x2)
+x = [0, 1]
 
-    return [x1, x2]
+checker = fsolve(NonLinearSystemSolver.function, x, fprime=NonLinearSystemSolver.jacobian_matrix, full_output=1)
+print("Scipy перевірка:", list(checker[0]))
 
+newton = Newton(x, 10e-4)
+print("Метод Ньютона:", newton.newton())
 
-x0 = [0, 1]
-cheat = fsolve(function, x0, fprime=jacobian_matrix, full_output=1)
-norm = newton(function, x0, jacobian_matrix, 10 ** -4)
-iteration = simple_iteration(0, 1, 10 ** -4)
-print("Перевірка:", cheat)
-print("Методом Ньютона:", norm)
-print("Методом простих ітерацій:", iteration)
+simple = SimpleIteration(x, 10e-4)
+print("Метод простих ітерацій:", simple.simple_iteration())
